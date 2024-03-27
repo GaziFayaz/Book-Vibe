b102723af31567e34810a9d81c151dd24db236d5
GaziFayaz <gazifayaz.16694@gmail.com>
2024-03-27 15:34:32 -0700

    partial sort context implementation

--- B9A8-Book-Review/src/Components/ListedBooks/ListedBooks.jsx
+++ B9A8-Book-Review/src/Components/ListedBooks/ListedBooks.jsx
@@ -1,55 +1,60 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { BiSolidChevronDown } from "react-icons/bi";
 import { NavLink, Outlet } from "react-router-dom";
+
+export const SortContext = createContext("rating");
 const ListedBooks = () => {
+	const [sortType, setSortType] = useState("rating");
 	const activeTab = "tab-active";
 	return (
-		<div className="flex flex-col items-center">
-			<header className="font-bold text-3xl text-center bg-[#1313130d] block py-[33px] w-full rounded-3xl">
-				Books
-			</header>
-			<details className="dropdown mt-5 mx-auto mb-[56px]">
-				<summary className="m-1 btn bg-default text-white">
-					<p className="text-lg font-semibold">Sort By</p>
-					<BiSolidChevronDown className="text-xl" />
-				</summary>
-				<ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 bg-gray-300">
-					<li>
-						<a>Rating</a>
-					</li>
-					<li>
-						<a>Number of Pages</a>
-					</li>
-					<li>
-						<a>Publisher Year</a>
-					</li>
-				</ul>
-			</details>
-			<div
-				role="tablist"
-				className="tabs tabs-lifted tabs-lg self-start w-full"
-			>
-				<NavLink
-					to={`read-books`}
-					role="tab"
-					className={({ isActive }) =>
-						isActive ? `active tab  font-bold ${activeTab}` : "tab "
-					}
+		<SortContext.Provider value={sortType}>
+			<div className="flex flex-col items-center">
+				<header className="font-bold text-3xl text-center bg-[#1313130d] block py-[33px] w-full rounded-3xl">
+					Books
+				</header>
+				<details className="dropdown mt-5 mx-auto mb-[56px]">
+					<summary className="m-1 btn bg-default text-white">
+						<p className="text-lg font-semibold">Sort By</p>
+						<BiSolidChevronDown className="text-xl" />
+					</summary>
+					<ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 bg-gray-300">
+						<li>
+							<a>Rating</a>
+						</li>
+						<li>
+							<a>Number of Pages</a>
+						</li>
+						<li>
+							<a>Publisher Year</a>
+						</li>
+					</ul>
+				</details>
+				<div
+					role="tablist"
+					className="tabs tabs-lifted tabs-lg self-start w-full"
 				>
-					Read Books
-				</NavLink>
-				<NavLink
-					to={`wishlist-books`}
-					role="tab"
-					className={({ isActive }) =>
-						isActive ? `active tab font-bold ${activeTab}` : "tab  "
-					}
-				>
-					Wishlist Books
-				</NavLink>
+					<NavLink
+						to={`read-books`}
+						role="tab"
+						className={({ isActive }) =>
+							isActive ? `active tab  font-bold ${activeTab}` : "tab "
+						}
+					>
+						Read Books
+					</NavLink>
+					<NavLink
+						to={`wishlist-books`}
+						role="tab"
+						className={({ isActive }) =>
+							isActive ? `active tab font-bold ${activeTab}` : "tab  "
+						}
+					>
+						Wishlist Books
+					</NavLink>
+				</div>
+				<Outlet></Outlet>
 			</div>
-			<Outlet></Outlet>
-		</div>
+		</SortContext.Provider>
 	);
 };
 
--- B9A8-Book-Review/src/Components/ReadBooks/ReadBooks.jsx
+++ B9A8-Book-Review/src/Components/ReadBooks/ReadBooks.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ListedBookCard from "../ListedBookCard/ListedBookCard";
+import { SortContext } from "../ListedBooks/ListedBooks";
 
 const ReadBooks = () => {
 	const [books, setBooks] = useState([]);
 	const [readBooksId, setReadBooksId] = useState([]);
+	const sortType = useContext(SortContext);
 
 	useEffect(() => {
+		console.log(sortType)
 		fetch("../books.json")
 			.then((res) => res.json())
 			.then((data) => setBooks(data));
@@ -13,10 +16,11 @@ const ReadBooks = () => {
 	useEffect(() => {
 		setReadBooksId(JSON.parse(localStorage.getItem("readBooks")));
 	}, []);
+	// useEffect(() => )
 
 	return (
 		<div className="flex flex-col w-full mt-10 gap-5">
-			{books.map((book,index) => {
+			{books.map((book, index) => {
 				if (readBooksId.includes(book.bookId)) {
 					return <ListedBookCard book={book} key={index}></ListedBookCard>;
 				}
